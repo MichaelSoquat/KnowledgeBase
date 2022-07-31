ad1e8bfe739f5da0c9f5b0bc01adbdf5fadd4c9a
MichaelSoquat <michael.soquat@gmx.de>
2022-07-31 17:42:05 +0200

    trying to install a widget

--- start/views.py
+++ start/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 from django.core import serializers
 
 from add.models import Knowledge
+from .forms import Input
 
 # Create your views here.
 def startView(request):
         data = Knowledge.objects.all()
-        
-        return render(request, 'start.html', {'knowledges': data})
+        form = Input
+        return render(request, 'start.html', {'knowledges': data, 'form': form})
